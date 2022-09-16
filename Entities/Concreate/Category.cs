26672cde835b6b20e020ba1d3ca313006df410b0
Yunus Emre Teke <yeteke68@gmail.com>
2022-09-16 22:14:27 +0300

    asdf

--- Entities/Concreate/Category.cs
+++ Entities/Concreate/Category.cs
@@ -7,12 +7,10 @@ using System.Threading.Tasks;
 
 namespace Entities.Concreate
 {
-    public class Product : IEntity
+    public class Category : IEntity
     {
-        public int ProductId { get; set; }
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
-        public short UnitsInStock { get; set; }
-        public decimal UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+
     }
 }
